545b3020fdca2144174b69bbf8c9003374f7bb92
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-07-28 21:42:37 +0300

    Add test for RegistrationController

---
+++ tests/Controller/RegistrationControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RegistrationControllerTest extends WebTestCase
+{
+    public function testRegistrationTemplate()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('post', '/registration');
+
+        $form = $crawler->selectButton('Зарегистрироваться')->form();
+
+        $form['firstName'] = 'Ivan';
+        $form['lastName'] = 'Ivanov';
+        $form['username'] = 'IvanovMaster';
+        $form['password'] = 'IvanovIvan';
+        $form['password_conf'] = 'IvanovIvan';
+        $form['country']->select('Ukraine');
+        $form['city']->select('Kiev');
+        $form['male']->tick();
+    }
+}
