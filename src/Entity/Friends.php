08beefb5dc185b917942a2e0ae5e304763e1449d
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-07-31 20:22:20 +0300

    Add Friends entity

---
+++ src/Entity/Friends.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FriendsRepository")
+ */
+class Friends
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $user_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $friend_id;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getFriendId(): ?int
+    {
+        return $this->friend_id;
+    }
+
+    public function setFriendId(int $friend_id): self
+    {
+        $this->friend_id = $friend_id;
+
+        return $this;
+    }
+}
