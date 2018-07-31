276d1b8cd40ac148edd19196195f6e6df9843127
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-07-31 20:21:13 +0300

    Add Cities entity

---
+++ src/Entity/Cities.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CitiesRepository")
+ */
+class Cities
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $country_id;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCountryId(): ?int
+    {
+        return $this->country_id;
+    }
+
+    public function setCountryId(int $country_id): self
+    {
+        $this->country_id = $country_id;
+
+        return $this;
+    }
+}
