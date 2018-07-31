58823b5f1e54e58aaa18a795e280245844f42688
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-07-31 20:25:23 +0300

    Add Messages entity

---
+++ src/Entity/Messages.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MessagesRepository")
+ */
+class Messages
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $sender_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $receiver_id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getSenderId(): ?int
+    {
+        return $this->sender_id;
+    }
+
+    public function setSenderId(int $sender_id): self
+    {
+        $this->sender_id = $sender_id;
+
+        return $this;
+    }
+
+    public function getReceiverId(): ?int
+    {
+        return $this->receiver_id;
+    }
+
+    public function setReceiverId(int $receiver_id): self
+    {
+        $this->receiver_id = $receiver_id;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
