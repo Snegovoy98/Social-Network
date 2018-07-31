9d046ac0109d738f333cf38eb5b9eb6210fbc8dc
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-07-31 20:27:25 +0300

    Add User entity

---
+++ src/Entity/User.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $surname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fathername;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $login;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_born;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $city_born_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $live_city_id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updated_at;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFathername(): ?string
+    {
+        return $this->fathername;
+    }
+
+    public function setFathername(string $fathername): self
+    {
+        $this->fathername = $fathername;
+
+        return $this;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getDateBorn(): ?\DateTimeInterface
+    {
+        return $this->date_born;
+    }
+
+    public function setDateBorn(\DateTimeInterface $date_born): self
+    {
+        $this->date_born = $date_born;
+
+        return $this;
+    }
+
+    public function getCityBornId(): ?int
+    {
+        return $this->city_born_id;
+    }
+
+    public function setCityBornId(int $city_born_id): self
+    {
+        $this->city_born_id = $city_born_id;
+
+        return $this;
+    }
+
+    public function getLiveCityId(): ?int
+    {
+        return $this->live_city_id;
+    }
+
+    public function setLiveCityId(int $live_city_id): self
+    {
+        $this->live_city_id = $live_city_id;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
