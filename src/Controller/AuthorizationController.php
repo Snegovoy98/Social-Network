2b2a3ea7471c7323e3f87e0b9a2bbfe05b731745
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-08-12 20:32:36 +0300

    Add AuthorizationController

---
+++ src/Controller/AuthorizationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AuthorizationController extends AbstractController
+{
+    public function index(AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('authorization/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+}
