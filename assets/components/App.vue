ed6313f15a83c22821829621807ad5cb6a60100d
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-08-12 20:11:56 +0300

    Add file Vue.js

---
+++ assets/components/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>This is an example of a new components in Symfony And Vue</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "example"
+    }
+</script>
