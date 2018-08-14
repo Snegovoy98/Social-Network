7e5deba1c1cd9c3979f9e0323ff60d66f45c1d81
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-08-14 18:21:08 +0300

    Refactor RegistrationType

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -8,7 +8,9 @@ use Symfony\Component\Form\Extension\Core\Type\{TextType,
                                                 PasswordType,
                                                 SubmitType,
                                                 BirthdayType,
-                                                CountryType
+                                                CountryType,
+                                                ChoiceType,
+                                                RadioType
                                                         };
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
@@ -20,48 +22,98 @@ class RegistrationType extends AbstractType
     {
         $builder
             ->add('lastName', TextType::class, [
+                'label' => 'Фамилия',
                 'attr' =>
-                    ['name' => 'lastName', 'id' => 'lastName'],
+                    ['name' => 'lastName',
+                     'id' => 'lastName',
+                      'class' => 'form-control',
+                      'placeholder' => 'Введите фамилию'
+                    ],
                 'constraints' => [new Length(['min' => 3, 'max' => 15]),
                     new NotBlank()
                 ]
                 ])
             ->add('firstName', TextType::class, [
+                'label' => 'Имя',
                 'attr' =>
-                    ['name' => 'firstName', 'id' => 'firstName'],
+                    ['name' => 'firstName',
+                     'id' => 'firstName',
+                     'class' => 'form-control',
+                     'placeholder' => 'Введите имя'
+                    ],
                 'constraints' => [new Length(['min' => 3, 'max' => 10]),
                     new NotBlank()
                 ]
             ])
             ->add('fatherName', TextType::class ,[
+                'label' => 'Отчество',
                 'attr' =>
-                    ['name' => 'fatherName', 'id' => 'fatherName'],
+                    ['name' => 'fatherName',
+                     'id' => 'fatherName',
+                     'class' => 'form-control',
+                     'placeholder' => 'Введите отчество'
+                    ],
                 'constraints' => [new Length(['min' => 6, 'max' => 15]),
                     new NotBlank()
                 ]
             ])
             ->add('login', TextType::class, [
+                'label' => 'Логин',
                 'constraints' => [new Length(['min' => 6, 'max' => 32]),
                                   new NotBlank()
-                ]
+                ],
+                'attr' =>['class' => 'form-control',
+                          'placeholder' => 'Введите логин']
             ])
             ->add('password', PasswordType::class, [
+                'label' => 'Пароль',
                 'constraints' => [new Length(['min' => 8, 'max' => 32]),
                     new NotBlank()
                 ],
-                'first_options'  => ['label' => 'Password'],
-                'second_options' => ['label' => 'Repeat Password']
+                'attr' =>['class'       => 'form-control',
+                          'placeholder' => 'Введите пароль']
             ])
             ->add('birthdate', BirthdayType::class, [
                 'widget' => 'single_text',
-                'format' => 'yyyy-MM-ddd'
+                'format' => 'yyyy-MM-dd',
+                'placeholder' => 'Select a value',
+                'attr'        => ['class' => 'custom-select d-block w-100',
+                                    'name' => ''],
+                'label'    => 'Дата рождения'
             ])
-            ->add('countries', CountryType::class)
-            ->add('submit', SubmitType::class)
+            ->add('country', CountryType::class, [
+                'placeholder' => 'Select a value',
+                'attr'        => ['class' => 'custom-select d-block w-100'],
+                'label'       => 'Страна'
+            ])
+            ->add('states', ChoiceType::class , [
+                'placeholder' => 'Select a value',
+                'choices' => [
+                    'Kabul' => 'AF'
+                ],
+                'attr'        => ['class' => 'custom-select d-block w-100'],
+            ])
+            ->add('gender', ChoiceType::class , [
+                'placeholder' => 'Select a value',
+                'choices' => [
+                    'Мужской' => 'male',
+                    'Женский' => 'female'
+                ],
+                'attr'        => ['class' => 'custom-select d-block w-100'],
+                'label' => 'Пол'
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary btn-lg btn-block',
+                            'value' => 'Зарегистрироваться']
+            ])
             ;
     }
     public function configureOptions(OptionsResolver $resolver)
     {
-
+        $resolver->setDefaults([
+            'error_mapping' => [
+                'matchingCityAndZipCode' => 'city',
+            ],
+        ]);
     }
 }
